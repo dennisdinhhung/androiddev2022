3f598578f3b18685689c3ab3761c78abc9932ef0
dennisdinhhung <dennisdinhhung@mgmail.com>
2021-10-06 10:56:42 +0700

    Add @Override and add Log.i()

---
+++ app/src/main/java/vn/edu/usth/usthweather/WeatherActivity.java
@@ -0,0 +1,44 @@
+package vn.edu.usth.usthweather;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+
+public class WeatherActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_weather);
+
+        Log.i("success", "Success!");
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+}
+
