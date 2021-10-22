82a00bfbd92d66360223407229aa389e26466b73
dennisdinhhung <dennisdinhhung@mgmail.com>
2021-10-22 21:14:59 +0700

    Lab 8: Finished adding TabLayout for ViewPager2

--- app/src/main/java/vn/edu/usth/usthweather/WeatherActivity.java
+++ app/src/main/java/vn/edu/usth/usthweather/WeatherActivity.java
@@ -2,17 +2,10 @@ package vn.edu.usth.usthweather;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
+
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentActivity;
-import androidx.fragment.app.FragmentManager;
-import androidx.fragment.app.FragmentPagerAdapter;
-import androidx.fragment.app.FragmentStatePagerAdapter;
-import androidx.recyclerview.widget.RecyclerView;
-import androidx.viewpager.widget.PagerAdapter;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.Fragment;
-import androidx.viewpager.widget.ViewPager;
+
 import androidx.viewpager2.adapter.FragmentStateAdapter;
 import androidx.viewpager2.widget.ViewPager2;
 
@@ -24,10 +17,13 @@ import android.util.Log;
 import android.view.View;
 import android.widget.LinearLayout;
 
+import com.google.android.material.tabs.TabLayout;
+
 public class WeatherActivity<activity, val> extends FragmentActivity {
     private static final int NUM_PAGES = 3;
     private ViewPager2 viewPager2;
     private FragmentStateAdapter pagerAdapter;
+    private TabLayout tabLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,6 +33,39 @@ public class WeatherActivity<activity, val> extends FragmentActivity {
         viewPager2 = findViewById(R.id.pager);
         pagerAdapter = new ViewPagerAdapter(this);
         viewPager2.setAdapter(pagerAdapter);
+
+        //Create new instance of the tab and display it
+        tabLayout = findViewById(R.id.tab);
+        tabLayout.addTab(tabLayout.newTab().setText("First"));
+        tabLayout.addTab(tabLayout.newTab().setText("Second"));
+        tabLayout.addTab(tabLayout.newTab().setText("Third"));
+
+        //For clicking the tab: change the tab accordingly
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                viewPager2.setCurrentItem(tab.getPosition());
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+
+        //For swiping: change the Tab accordingly to the current page view
+        viewPager2.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback() {
+            @Override
+            public void onPageSelected(int position) {
+                tabLayout.selectTab(tabLayout.getTabAt(position));
+            }
+        });
+
     }
 
     @Nullable
@@ -45,6 +74,7 @@ public class WeatherActivity<activity, val> extends FragmentActivity {
         return super.onCreateView(name, context, attrs);
     }
 
+    //Adapter for ViewPager2
     public class ViewPagerAdapter extends FragmentStateAdapter{
         public ViewPagerAdapter(FragmentActivity fa){
             super(fa);
