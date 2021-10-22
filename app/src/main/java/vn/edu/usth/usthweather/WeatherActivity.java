56575378538d4caaa92ba8a83254a434c8c4f993
dennisdinhhung <dennisdinhhung@mgmail.com>
2021-10-22 17:11:17 +0700

    Lab 7: Finished added ViewPager2 in the Activity, with FailFragment (small bug: no header in view)

--- app/src/main/java/vn/edu/usth/usthweather/WeatherActivity.java
+++ app/src/main/java/vn/edu/usth/usthweather/WeatherActivity.java
@@ -4,6 +4,18 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.fragment.app.FragmentStatePagerAdapter;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.viewpager.widget.PagerAdapter;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.viewpager.widget.ViewPager;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+import androidx.viewpager2.widget.ViewPager2;
+
 
 import android.content.Context;
 import android.os.Bundle;
@@ -12,26 +24,50 @@ import android.util.Log;
 import android.view.View;
 import android.widget.LinearLayout;
 
-public class WeatherActivity extends AppCompatActivity {
+public class WeatherActivity<activity, val> extends FragmentActivity {
+    private static final int NUM_PAGES = 3;
+    private ViewPager2 viewPager2;
+    private FragmentStateAdapter pagerAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_weather);
 
-        Log.i("onCreate()", "Successfully done onCreate()!");
-
-        ForecastFragment firstFragment = new ForecastFragment();
-        WeatherFragment weatherFragment = new WeatherFragment();
-
-        getSupportFragmentManager().beginTransaction().add(R.id.id_weather_activity, weatherFragment).commit();
-        getSupportFragmentManager().beginTransaction().add(R.id.id_weather_activity, firstFragment).commit();
+        viewPager2 = findViewById(R.id.pager);
+        pagerAdapter = new ViewPagerAdapter(this);
+        viewPager2.setAdapter(pagerAdapter);
     }
 
     @Nullable
     @Override
     public View onCreateView(@NonNull String name, @NonNull Context context, @NonNull AttributeSet attrs) {
         return super.onCreateView(name, context, attrs);
+    }
+
+    public class ViewPagerAdapter extends FragmentStateAdapter{
+        public ViewPagerAdapter(FragmentActivity fa){
+            super(fa);
+        }
+
+        @NonNull
+        @Override
+        public Fragment createFragment(int position) {
+            switch (position){
+                case 0:
+                    return new WeatherForecastFragment();
+                case 1:
+                    return new WeatherForecastFragment();
+                case 2:
+                    return new WeatherForecastFragment();
+            }
+            return new FailFragment();
+        }
+
+        @Override
+        public int getItemCount() {
+            return NUM_PAGES;
+        }
     }
 
     @Override
